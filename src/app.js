56a6431cc2f0943938ca81538f69367eaa242cd5
Jagoda Skubis <33773@humanitas365.edu.pl>
2023-05-26 19:24:29 +0200

    witaj-swiecie

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/test", (_req, res) => {
-    res.status(200).send("Hello World")
+    res.status(200).send("Witaj Świecie")
 })
 
 module.exports = app;
